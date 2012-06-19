10d51a57718464f75c307a4cb863565d36c62446
Marc Culler <culler@users.noreply.github.com>
2012-06-19 00:44:34 -0500

    Made cypari not zip-safe, so py2app can find it.

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup, Command
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-import os
+import os, sys
+pari_ver = 'pari-2.5.1'
+pari_include_dir = os.path.join(pari_ver, 'include')
+pari_library_dir = os.path.join(pari_ver, 'lib')
+pari_library = os.path.join(pari_library_dir, 'libpari.a')
+command = sys.argv[1]
 
-pari_include_dir = ['.', 'pari-2.5.1/include']
-pari_gen = Extension('cypari.gen',
-                     sources = ['cypari/gen.pyx'],
-                     include_dirs = pari_include_dir,
-                     library_dirs = ['pari-2.5.1/lib'],
-                     libraries = ['pari'])
-
+if ( command in ['build', 'build_ext', 'install', 'bdist_egg'] and
+     not os.path.exists(pari_library) ):
+    os.system('sh build_pari.sh')
+    
 class clean(Command):
     user_options = []
     def initialize_options(self):
@@ -22,10 +24,16 @@ class clean(Command):
         os.system('rm -rf cypari*.egg-info')
         os.system('rm -f cypari/gen.c cypari/gen.h cypari/*.pyc')
 
+pari_gen = Extension('cypari.gen',
+                     sources = ['cypari/gen.pyx'],
+                     include_dirs = [pari_include_dir],
+                     library_dirs = [pari_library_dir],
+                     libraries = ['pari'])
 
 setup(
   name = 'cypari',
   version = '1.0',
+  zip_safe = False,
   packages = ['cypari'],
   cmdclass = {'build_ext': build_ext, 'clean':clean},
   ext_modules = [pari_gen],
