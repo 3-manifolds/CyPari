2f98c7033dd03413f0d8ae35763e6c6f03e2135c
Marc Culler <culler@users.noreply.github.com>
2012-06-19 15:15:45 -0500

    Python 3 compatibility.  No __long__ and dots in the module names.

--- cypari/all.py
+++ cypari/all.py
@@ -1,4 +1,4 @@
-import gen as _gen
+from . import gen as _gen
 
 pari = _gen.pari
 
@@ -8,6 +8,6 @@ allocatemem = pari.allocatemem
 
 PariError = _gen.PariError
 
-from gen import (prec_dec_to_words, prec_dec_to_bits,
+from .gen import (prec_dec_to_words, prec_dec_to_bits,
                  prec_bits_to_words, prec_bits_to_dec,
                  prec_words_to_bits, prec_words_to_dec)
