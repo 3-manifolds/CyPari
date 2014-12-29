109a7ebf828d7bbb1edb5d29ab8daf6aab27dd53
Nathan Dunfield <nathan@dunfield.info>
2014-12-28 18:28:18 -0600

    Moving source around

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-from setuptools import setup, Command
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from setuptools import setup
+import setuptools, setuptools.command.sdist, os, sys
 
-import os, sys
 pari_ver = 'pari-2.5.5'
 pari_include_dir = os.path.join(pari_ver, 'include')
 pari_library_dir = os.path.join(pari_ver, 'lib')
@@ -15,7 +13,7 @@ if not os.path.exists(pari_library) and 'clean' not in sys.argv:
     if os.system('bash build_pari.sh') != 0:
         sys.exit("***Failed to build PARI library***")
     
-class clean(Command):
+class clean(setuptools.Command):
     user_options = []
     def initialize_options(self):
         pass 
@@ -24,14 +22,30 @@ class clean(Command):
     def run(self):
         os.system('rm -rf build dist')
         os.system('rm -rf cypari*.egg-info')
-        os.system('rm -f cypari/gen.c cypari/gen.h cypari/*.pyc')
+        os.system('rm -f cypari_src/gen.c cypari_src/gen.h cypari_src/*.pyc')
 
-pari_gen = Extension('cypari.gen',
-                     sources=['cypari/gen.pyx'],
-                     include_dirs=[pari_include_dir],
+class sdist(setuptools.command.sdist.sdist):
+    def run(self):
+        from Cython.Build import cythonize
+        cythonize(['cypari_src/gen.pyx'])
+        setuptools.command.sdist.sdist.run(self)
+        
+
+cmdclass = {'clean':clean, 'sdist':sdist}
+
+try:
+    from Cython.Distutils import build_ext
+    have_cython, source_ext = True, '.pyx'
+    cmdclass['build_ext'] = build_ext
+except ImportError:
+    have_cython, source_ext = False, '.c'
+    
+
+pari_gen = setuptools.Extension('cypari.gen',
+                     sources=['cypari_src/gen' + source_ext],
+                     include_dirs=['cypari_src', pari_include_dir],
                      library_dirs=[pari_library_dir],
                      libraries=['pari', 'm'],
-#                     language = 'c++'
                      )
 
 setup(
@@ -39,7 +53,8 @@ setup(
   version = '1.2',
   zip_safe = False,
   packages = ['cypari'],
-  cmdclass = {'build_ext': build_ext, 'clean':clean},
+  package_dir = {'cypari':'cypari_src'}, 
+  cmdclass = cmdclass,
   ext_modules = [pari_gen],
   author = 'Marc Culler and Nathan Dunfield',
   author_email = 'culler@math.uic.edu, nmd@illinois.edu',
@@ -48,3 +63,4 @@ setup(
   keywords = 'Pari, Sage, SnapPy',
   url = 'http://www.math.uic.edu/t3m',
 )
+
