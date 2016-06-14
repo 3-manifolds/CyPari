035dd66afe9e303318ecd24a37afc052c5506720
Marc Culler <culler@users.noreply.github.com>
2016-06-14 16:49:38 -0500

    Updated build_pari.sh for macOS aka OS X.  This builds and runs on a 64 bit Mac.

--- Version2/build_pari.sh
+++ Version2/build_pari.sh
@@ -16,33 +16,19 @@ cd pari_src
 
 echo "Building Pari libary..." 
 if [ "$(uname)" = "Darwin" ] ; then  # OS X
-    export CFLAGS='-arch i386 -mmacosx-version-min=10.4 '
-    ./Configure --prefix=${PREFIX} --without-gmp --host=i386-darwin
-    cd Odarwin-i386
-    make install-lib-sta
-    make install-include
-    cd ..
-    mv lib/libpari.a lib/i386-libpari.a
-    mv include include32
-#
     export CFLAGS='-arch x86_64'
-    ./Configure --prefix=`pwd` --without-gmp --host=x86_64-darwin
+    ./Configure --prefix=${PREFIX} --without-gmp --host=x86_64-darwin
     cd Odarwin-x86_64
+    make install
     make install-lib-sta
-    make install-include
-    cd ..
-    mv lib/libpari.a lib/x86_64-libpari.a
-    mv include include64
-#
-    lipo lib/i386-libpari.a lib/x86_64-libpari.a -create -output lib/libpari.a
-    ranlib lib/*.a
-    ln -s include32 include
+    cd ../..
+    cp pari_src/src/language/anal.h pari/include/pari
 
 elif [ "$(uname)" = *MINGW32* ] ; then # MinGW on Windows
     ./Configure --prefix=${PREFIX} --libdir=lib --without-gmp --host=i386-mingw
     cd Omingw-i386
+    make install
     make install-lib-sta
-    make install-include
 else  # Linux
     ./Configure --prefix=${PREFIX} --without-gmp
     cd Olinux-*
@@ -50,6 +36,6 @@ else  # Linux
     make install-lib-sta
     cd ../..
     cp pari_src/src/language/anal.h pari/include/pari
-    cd pari
+#    cd pari
 #    ln -s lib/libpari.a libpari.a
 fi
