56dc83991dc84a50536cf410378c2956110f7b5e
Nathan Dunfield <nathan@dunfield.info>
2016-06-15 08:41:37 -0600

    Added basic testing module

---
+++ Version2/cypari/test.py
@@ -0,0 +1,20 @@
+import doctest
+import re
+from . import tests
+from .all import pari
+
+class DocTestParser(doctest.DocTestParser):
+    def parse(self, string, name='<string>'):
+        string, num = re.subn('([\n\A]\s*)sage:', '\g<1>>>>', string)
+        string, num = re.subn('....:', '...', string)
+        string, num = re.subn('\^', '**', string)
+        return doctest.DocTestParser.parse(self, string, name)
+
+if __name__ == '__main__':
+    finder = doctest.DocTestFinder(parser=DocTestParser())
+    extra_globals = dict([('pari',pari)])
+    runner = doctest.DocTestRunner(verbose=False)
+    for test in finder.find(tests, extraglobs=extra_globals):
+        runner.run(test)
+    print runner.summarize()
+
