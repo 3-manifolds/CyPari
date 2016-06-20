d27cebc1f7b6f0babcf00832f66612ec67715632
Nathan Dunfield <nathan@dunfield.info>
2016-06-20 10:04:56 -0600

    Added verbose '-v' flag to cypari.test

--- Version2/cypari/test.py
+++ Version2/cypari/test.py
@@ -1,7 +1,5 @@
-import doctest
-import re
+import doctest, re, getopt, sys
 from . import tests
-from . import pari
 from . import gen
 from . import pari_instance
 
@@ -11,12 +9,26 @@ class DocTestParser(doctest.DocTestParser):
         string, num = re.subn('\.\.\.\.:', '...', string)
         return doctest.DocTestParser.parse(self, string, name)
 
+extra_globals = dict([('pari', pari_instance.pari)])    
+modules_to_test = [
+    (tests, extra_globals),
+    (pari_instance, extra_globals),
+    # (gen, extra_globals)
+]
+
+
 if __name__ == '__main__':
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:], 'v', ['verbose'])
+        opts = [o[0] for o in optlist]
+        verbose = '-v' in opts
+    except getopt.GetoptError:
+        verbose = False
+    
     finder = doctest.DocTestFinder(parser=DocTestParser())
-    extra_globals = dict([('pari',pari)])
     failed, attempted = 0, 0
-    runner = doctest.DocTestRunner(verbose=False)
-    for module in [tests, pari_instance]:   # Adding gen causes segfault on OS X
+    runner = doctest.DocTestRunner(verbose=verbose)
+    for module, extra_globals in modules_to_test:
         for test in finder.find(module, extraglobs=extra_globals):
             runner.run(test)
         result = runner.summarize()
