9ed38aeb8b15a3b2115f5b0aa857f0e3faf6b727
Marc Culler <culler@users.noreply.github.com>
2016-12-14 12:41:26 -0600

    Adjustments to the build process on macOS.

--- Version2/build_pari.sh
+++ Version2/build_pari.sh
@@ -21,18 +21,16 @@ if [ "$(uname)" = "Darwin" ] ; then  # OS X
     ./Configure --prefix=${PREFIX} --without-gmp --host=x86_64-darwin
     cd Odarwin-x86_64
     make install
+    make install-lib-sta
     cd ../..
     cp pari_src/src/language/anal.h pari/include/pari
-    cd ../cypari
-    if [ ! -e "libpari-2.8.dylib" ] ; then
-	ln -s ../build/pari/lib/libpari-2.8.dylib
-    fi
 
 elif [ "$(uname)" = *MINGW32* ] ; then # MinGW on Windows
     ./Configure --prefix=${PREFIX} --libdir=lib --without-gmp --host=i386-mingw
     cd Omingw-i386
     make install
     make install-lib-sta
+
 else  # Linux
     ./Configure --prefix=${PREFIX} --without-gmp
     cd Olinux-*
@@ -40,8 +38,4 @@ else  # Linux
     make install-lib-sta
     cd ../..
     cp pari_src/src/language/anal.h pari/include/pari
-    cd ../cypari
-    if [ ! -e "libpari-2.8.so.0" ] ; then
-	ln -s ../build/pari/lib/libpari-2.8.so.0 .
-    fi
 fi
--- Version2/cypari/paridecl.pxd
+++ Version2/cypari/paridecl.pxd
@@ -75,7 +75,7 @@ cdef extern from "parisage.h":
     GEN     ghalf
     GEN     gen_0
     GEN     gnil
-    int     PARI_SIGINT_block, PARI_SIGINT_pending
+#    int     PARI_SIGINT_block, PARI_SIGINT_pending
     void    NEXT_PRIME_VIADIFF(long, byteptr)
     void    PREC_PRIME_VIADIFF(long, byteptr)
     int     INIT_JMPm, INIT_SIGm, INIT_DFTm, INIT_noPRIMEm, INIT_noIMTm
