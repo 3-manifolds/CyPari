2a0d720b099a7725a72f7e8e4d6c189e95a498b5
Marc Culler <Marc Culler@localhost>
2016-12-23 22:41:15 -0600

    Try using the full path for wmic.

--- Version2/cypari/memory.py
+++ Version2/cypari/memory.py
@@ -1,6 +1,8 @@
 from sys import platform
 from subprocess import Popen, PIPE
 
+wmic = 'C:\Windows\System32\wbem\wmic'
+
 def total_ram():
     if platform == 'linux2':
         out, err = Popen(['free', '-b'], stdout=PIPE).communicate()
@@ -13,7 +15,7 @@ def total_ram():
         out, err = Popen(['sysctl', 'hw.memsize'], stdout=PIPE).communicate()
         return int(out.split()[1])
     elif platform == 'win32':
-         proc = Popen(['wmic', 'computersystem', 'get', 'TotalPhysicalMemory'],
+         proc = Popen([wmic, 'computersystem', 'get', 'TotalPhysicalMemory'],
                       stdout=PIPE)
          out, err = proc.communicate()
          return int(out.split()[1])
