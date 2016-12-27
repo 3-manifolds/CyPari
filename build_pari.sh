80e2fd5d047866ec0c357a195f43858a4451b076
Nathan Dunfield <nathan@dunfield.info>
2016-12-26 19:50:53 -0600

    Try running Pari's test suite

--- build_pari.sh
+++ build_pari.sh
@@ -40,6 +40,7 @@ elif [[ "$(uname)" = *MINGW32* ]] || [[ "$(uname)" = *MSYS* ]]; then # MinGW on 
     cd Omingw-i386
     make install-lib-sta
     make install-include
+    make test-all
 else  # Linux
     ./Configure --prefix=`pwd` --without-gmp
     cd Olinux-*
