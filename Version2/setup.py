ce5c4be6055cc6b14adc3d0989dab0b6d0fe4da2
Marc Culler <culler@users.noreply.github.com>
2017-01-16 19:25:20 -0600

    Also try using a Windows path.

--- Version2/setup.py
+++ Version2/setup.py
@@ -251,7 +251,7 @@ link_args = []
 compile_args = []
 if sys.platform == 'win32':
     if sys.version_info.major > 2:
-        link_args = ['/c/Windows/System32/Python34.dll', '-specs=specs100']
+        link_args = [r'C:\Windows\System32\Python34.dll', '-specs=specs100']
     else:
         link_args = ['-specs=specs90']
     link_args += ['-Wl,--subsystem,windows']
