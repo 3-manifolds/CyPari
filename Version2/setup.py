afd414f0cacabfa782948f91780ab209055e3645
Nathan Dunfield <nathan@dunfield.info>
2017-01-23 13:43:46 -0600

    Updated link to Sage component

--- Version2/setup.py
+++ Version2/setup.py
@@ -3,7 +3,7 @@ The package *cypari* is a Python wrapper for the `PARI library
 <http://pari.math.u-bordeaux.fr/>`_, a computer algebra system for
 number theory computations.  It is derived from the `corresponding
 component
-<http://doc.sagemath.org/html/en/reference/libs/sage/libs/pari/index.html>`_
+<https://github.com/sagemath/sage/blob/797dd7b4c273556d9677fadffa2ef6dd7f113857/src/sage/libs/cypari2/gen.pyx>`_
 of `SageMath <http://www.sagemath.org>`_, but is independent of the rest of
 SageMath and can be used with any recent version of Python (except on Windows,
 where 3.4 is currently the only supported version of Python 3).
