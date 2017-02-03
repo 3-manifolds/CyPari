2a81fe037d36132bfa48739b4564cb323794d68a
Marc Culler <marc.culler@gmail.com>
2017-02-03 06:53:18 -0600

    Minor adjustment to top level imports.  The name 'gen' for the cypari extension module is not particularly intuitive, since it defines classes which are not subsidiary to gen.  So we should provide the basic classes and utilities at the top level, making imports from cypari.gen unnecessary. However, for backwards compatibility we should retain the name.

--- Version2/cypari_src/__init__.py
+++ Version2/cypari_src/__init__.py
@@ -1,5 +1,7 @@
-__all__ = ['pari']
 from .gen import gen as pari_gen, pari, PariError
-
+# These should be sufficient for most people.
+__all__ = ['pari_gen', 'pari', 'PariError']
+# But these might come in handy for tinkerers.
+from .gen import (prec_words_to_dec, prec_words_to_bits, prec_bits_to_dec, prec_dec_to_bits)
 
 
