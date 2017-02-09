1bc66197edc689725b889572717de61ad8ab0a80
Nathan Dunfield <nathan@dunfield.info>
2017-02-09 20:46:46 +0000

    Updated package description

--- Version2/setup.py
+++ Version2/setup.py
@@ -5,8 +5,7 @@ number theory computations.  It is derived from the `corresponding
 component
 <https://github.com/sagemath/sage/blob/797dd7b4c273556d9677fadffa2ef6dd7f113857/src/sage/libs/cypari2/gen.pyx>`_
 of `SageMath <http://www.sagemath.org>`_, but is independent of the rest of
-SageMath and can be used with any recent version of Python (except on Windows,
-where 3.4 is currently the only supported version of Python 3).
+SageMath and can be used with any recent version of Python 2 or 3.
 """
 import os, sys, re, sysconfig, subprocess, shutil, site, platform
 from glob import glob
