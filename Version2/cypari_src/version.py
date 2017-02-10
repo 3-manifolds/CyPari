f378f824185cb94a9c1e76d1f66c7cd1505d57ef
Marc Culler <Marc Culler@localhost>
2017-02-10 15:43:09 +0000

    Bumped version to 2.0.2.

--- Version2/cypari_src/version.py
+++ Version2/cypari_src/version.py
@@ -3,6 +3,6 @@ class Version(namedtuple('Version', ['major', 'minor', 'micro', 'tag'])):
     def __str__(self):
         return '%s.%s.%s%s'%self
     
-version_info = Version(2, 0, 1, '')
+version_info = Version(2, 0, 2, '')
 __version__ = str(version_info)
         
