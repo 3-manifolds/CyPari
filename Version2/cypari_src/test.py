5d397cbf5d3cbaa6b2ff46ba5c148db9cc4f1152
Marc Culler <marc.culler@gmail.com>
2017-03-11 10:33:08 -0600

    Modified tests.py so we run doctests with prompt 'sage:'.

--- Version2/cypari_src/test.py
+++ Version2/cypari_src/test.py
@@ -10,17 +10,25 @@ else:
 cpu_width = '64bit' if sys.maxsize > 2**32 else '32bit'
 
 class DocTestParser(doctest.DocTestParser):
+
     def parse(self, string, name='<string>'):
+        # Remove tests for the wrong architecture
         regex32 = re.compile(r'(\n.*?)\s+# 32-bit\s*$', re.MULTILINE)
         regex64 = re.compile(r'(\n.*?)\s+# 64-bit\s*$', re.MULTILINE)
         if cpu_width == '64bit':
             string = regex32.sub('', string)
-            string = regex64.sub('\g<1>', string)
+            string = regex64.sub('\g<1>\n', string)
         else:
             string = regex64.sub('', string)
-            string = regex32.sub('\g<1>', string)
+            string = regex32.sub('\g<1>\n', string)
+        # Adjust the name of the PariError exception
         if sys.version_info.major < 3:
+            ## Why does this fail with re.MULTILINE????
             string = re.sub('cypari_src.gen.PariError', 'PariError', string)
+        # Enable sage tests
+            string = re.sub('sage:', '>>>', string, re.MULTILINE)
+        # Remove lines containing :: which confuse doctests
+            string = re.sub('::', '                  ', string, re.MULTILINE)
         return doctest.DocTestParser.parse(self, string, name)
 
 extra_globals = dict([('pari', gen.pari)])    
@@ -30,6 +38,16 @@ modules_to_test = [
     (tests, extra_globals),
 ]
 
+# Cython adds a docstring to gen.__test__ *only* if it contains '>>>'.
+# To enable running Sage doctests, with prompt 'sage:' we need to add
+# some docstrings to gen.__test__
+
+for cls in (gen.Gen, gen.Pari):
+    for key, value in cls.__dict__.items():
+        docstring = getattr(cls.__dict__[key], '__doc__')
+        if isinstance(docstring, str):
+            if docstring.find('Sage:') >= 0 and docstring.find('>>>') < 0:
+                gen.__test__['%s.%s'%(cls.__name__, key)] = docstring
 
 def runtests(verbose=False):
     finder = doctest.DocTestFinder(parser=DocTestParser())
