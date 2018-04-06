3096214f53c4561a676349f652f97bba68bf0eaa
Marc Culler <Marc Culler@localhost>
2018-04-05 20:50:20 -0500

    Appveyor is using rev1 even though rev2 is the only available download.

--- Version2/Windows/toolchain/install_files_64.sh
+++ Version2/Windows/toolchain/install_files_64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GCCVERSION="6.3.0"
-TOOLCHAIN="/c/mingw-w64/x86_64-6.3.0-posix-dwarf-rt_v5-rev2/mingw64"
+TOOLCHAIN="/c/mingw-w64/x86_64-6.3.0-posix-dwarf-rt_v5-rev1/mingw64"
 LIBDIR_MINGW64="${TOOLCHAIN}/lib/"
 SPECSDIR_MINGW64="${TOOLCHAIN}/lib/gcc/x86_64-w64-mingw64/${GCCVERSION}/"
 cp mingw64/x86_64-w64-mingw32/lib/* ${LIBDIR_MINGW64}
