8229bd4d18c9383c1d62a9c70484e02a930a214a
Marc Culler <culler@users.noreply.github.com>
2018-04-05 20:19:05 -0500

    Modified the 32-bit build system to use a toolchain that Appveyor is currently supporting.

--- Version2/Windows/toolchain/install_files_32.sh
+++ Version2/Windows/toolchain/install_files_32.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 GCCVERSION=`gcc -dumpversion`
-LIBDIR_MINGW32="/c/msys64/mingw32/i686-w64-mingw32/lib/"
-SPECSDIR_MINGW32="/c/msys64/mingw32/lib/gcc/i686-w64-mingw32/${GCCVERSION}/"
+TOOLCHAIN="/c/mingw-w64/i686-6.3.0-posix-dwarf-rt_v5-rev2/mingw32"
+LIBDIR_MINGW32="${TOOLCHAIN}/lib/"
+SPECSDIR_MINGW32="${TOOLCHAIN}/lib/gcc/i686-w64-mingw32/${GCCVERSION}/"
 cp mingw32/i686-w64-mingw32/lib/* ${LIBDIR_MINGW32}
 cp specs90_32 ${SPECSDIR_MINGW32}/specs90
 cp specs100_32 ${SPECSDIR_MINGW32}/specs100
