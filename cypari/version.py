3ff9afb84cfb8836be2d74dbd5bdcb5706745994
Nathan Dunfield <nathan@dunfield.info>
2021-11-03 15:47:06 -0500

    Rollback of version number to see if can build on Windows

--- cypari/version.py
+++ cypari/version.py
@@ -3,6 +3,6 @@ class Version(namedtuple('Version', ['major', 'minor', 'micro', 'tag'])):
     def __str__(self):
         return '%s.%s.%s%s'%self
     
-version_info = Version(2, 5, 0, 'a1')
+version_info = Version(2, 4, 1)
 __version__ = str(version_info)
         
