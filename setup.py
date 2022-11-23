ac37b55d1b017614cb41d288c3a38b186e7f5ea2
Marc Culler <culler@users.noreply.github.com>
2022-11-23 08:48:21 -0700

    Update long_description

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ long_description =  """\
 The package *cypari* is a Python wrapper for the `PARI library
 <http://pari.math.u-bordeaux.fr/>`_, a computer algebra system for
 number theory computations.  It is derived from the `corresponding
-component
-<https://github.com/sagemath/sage/blob/797dd7b4c273556d9677fadffa2ef6dd7f113857/src/sage/libs/cypari2/gen.pyx>`_
+component <https://github.com/sagemath/cypari2>`_
 of `SageMath <http://www.sagemath.org>`_, but is independent of the rest of
-SageMath and can be used with any recent version of Python 2 or 3.
+SageMath and can be used with any recent version of Python 3.
 """
 
 no_cython_message = """
