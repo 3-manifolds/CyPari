94aaf0fd4c1d9eb3133de555957df4c5546f1f0e
Marc Culler <culler@users.noreply.github.com>
2023-03-06 09:14:32 -0600

    Declare old_handler for Windows.

--- cypari/macros.h
+++ cypari/macros.h
@@ -273,6 +273,9 @@ static inline void sig_retry(void)
  */
 static inline void sig_error(void)
 {
+#if defined(__MINGW32__) || defined(_WIN32)
+    void (*old_handler)(int);
+#endif
     DEBUG( "sig_error called with count %d\n", cysigs.sig_on_count)
     if (unlikely(cysigs.sig_on_count <= 0))
     {
