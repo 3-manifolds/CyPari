ea508f1094fa51b6335eb51a7b74818c07e13a7c
Marc Culler <culler@users.noreply.github.com>
2023-11-03 19:55:45 -0500

    Drop support for 32 bit systems and python < 3.6; use the ucrt64 toolchain for Windows.

--- Windows/toolchain/install_files_64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-GCCVERSION="6.3.0"
-TOOLCHAIN="/c/mingw-w64/x86_64-6.3.0-posix-seh-rt_v5-rev1/mingw64"
-LIBDIR_MINGW64="${TOOLCHAIN}/lib/"
-SPECSDIR_MINGW64="${TOOLCHAIN}/lib/gcc/x86_64-w64-mingw32/${GCCVERSION}/"
-cp mingw64/x86_64-w64-mingw32/lib/* ${LIBDIR_MINGW64}
-cp specs90_64 ${SPECSDIR_MINGW64}/specs90
-cp specs100_64 ${SPECSDIR_MINGW64}/specs100
