afcbd945097caa55ff89cfdc4c8949e188a890dd
Marc Culler <culler@users.noreply.github.com>
2026-09-22 19:50:05 -0500

    Another try at running autogen with msysy2 python.

--- autogen/paths.py
+++ autogen/paths.py
@@ -28,15 +28,12 @@ if not os.path.exists(gphelp):
     raise RuntimeError('gphelp not found at %s' % gphelp)
 
 prefix = os.path.join('libcache', PARIDIR)
-perl = 'perl'
-
-#Now we are using python3 from the msys distribution, so this is not needed.
-#if sys.platform == 'win32':
-#    perl = shutil.which('perl') or r'C:\msys64\usr\bin\perl.exe'
-#    if not os.path.exists(perl):
-#        raise RuntimeError('perl not found at %s' % perl)
-#else:
-#    perl = 'perl'
+if sys.platform == 'win32':
+    perl = r'C:\msys64\usr\bin\perl.exe'
+    if not os.path.exists(perl):
+        raise RuntimeError('perl not found at %s' % perl)
+else:
+    perl = 'perl'
 
 def pari_share():
     r"""
